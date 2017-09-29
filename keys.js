d943ff42abcdb01db1d1c2e80100ab595ec8428e
Bill Sorrentino <williamsorrentino@gmail.com>
2017-09-29 11:14:51 -0400

    created files for the application

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 't3Pfn5U7HzR7mOW1Bhsw45AiC',
+  consumer_secret: 'EvEMEEMkrkNK5EWixt5YDzPazup2cThEKLTTOO9fr2xFbYnekD',
+  access_token_key: '23328144-Z0t7xfffy5eP0y3i0wXRYq2pAN17WVUJPa2YPVNxK',
+  access_token_secret: '4owxxJu4wozFVh6OLKw3PrDExxD1VYIt002oPTg3gFkUs',
+}
+
+module.exports = twitterKeys;
