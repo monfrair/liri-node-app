8636ad374c53f37ff0b643f70efaa769db7946fe
Bill Sorrentino <williamsorrentino@gmail.com>
2017-09-30 08:35:46 -0400

    more updates to js files

--- liri.js
+++ liri.js
@@ -6,136 +6,97 @@
 
 
 // NPM Package for reading and writing files
-var fs = require('fs'); 
+var fs = require('fs');
 
 var dataKeys = require("./keys.js");
 var twitter = require("twitter");
-var spotifySong = require('node-spotify-api');
+var spotify = require("node-spotify-api");
 var request = require("request");
 
+
+///capture user input, and inform user of what to type in.
+console.log("Please Type my-tweets, spotify-this-song, movie-this, or do-what-it-says");
 
 //take in all the command line arguments
 var nodeArgs = process.argv;
 
 
+
 // Command Instructions possibilities: my-tweets, spotify-this-song, movie-this, do-what-it-says
-	var commandLine1 = process.argv[2];
-	var commandLine2 = process.argv[3];	
-	var commandLine3 = process.argv[4];
+var userChoice = process.argv[2];
+var userValue = process.argv[3];  //will this handle the whole name of the song if multiple words?
 
-//twitter variables
+for(i=4; i<process.argv.length; i++){
+	    userValue += '+' + process.argv[i];
+	}
+console.log(userValue);
+console.log(userChoice);
 
-var tweets = "";
-var userName = "monfrair";
-
-//spotify variables
-
-var artist = "";
-var songName = "";
-var link = "";
-var album ="";
-
-//movie variables
-
-var movieTitle = "";
-var year = 0;
-var imdbRating = 0;
-var country = "";
-var language = "";
-var plot = "";
-var actors = "";
-var rotten = "";
-var rottenURL = "";
-
-
-
-
-search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
-
-
-
- 
-var spotify = new Spotify({
-  id: 'cee40f14b4714e95a4942522c241ced1',
-  secret: '100ea93662344e14992be21b7a6d0b96'
+var spotifyThisSong = new spotify({
+    id: 'cee40f14b4714e95a4942522c241ced1',
+    secret: '100ea93662344e14992be21b7a6d0b96',
 });
 
 
- 
-spotify.search({ type: 'track', query: songName, limit: 20 }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-    
-var songs = data.tracks.items;
-    var data = []; //empty array to hold data
+//case switch function to determine users choice  change to an if else statement
+function userEntry(){
+	
+	switch(userChoice){
 
-    for (var i = 0; i < songs.length; i++) {
-      data.push({
-        'artist(s)': songs[i].artists.map(getArtistNames),
-        'song name: ': songs[i].name,
-        'preview song: ': songs[i].preview_url,
-        'album: ': songs[i].album.name,
-      });
-    }
-    console.log(data);
-   
-  });
+		case 'my-tweets':
+		fetchTweets();
+		break;
+
+		case 'spotify-this-song':
+		getSpotify();
+		break;
+
+		case 'movie-this':
+		aMovieForMe();
+		break;
+
+		case 'do-what-it-says':
+		followTheTextbook();
+		break;
+		
+        default: 
+            console.log("Please choose one of the following options: my-tweets,  spotify-this-song,  movie-this, or  do-what-it-says");
+	}
 };
 
 
 //Function for finding songs on Spotify
-var getSpotify = function(songName) {
-  //If it doesn't find a song, default is The Sign by Ace of Base
-  if (songName === undefined) {
-    songName = 'The Sign';
-  };
+function getSpotify() {
+    //If it doesn't find a song, default is The Sign by Ace of Base
+    if (userValue === undefined) {
+        userValue = 'The Sign';
+    };
+    
 
-  spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if (err) {
-      console.log('Spotify Error occurred: ' + err);
-      return;
-    }
+    //spotify variables for user account
 
-    var songs = data.tracks.items;
-    var data = []; //empty array to hold data
+   spotify.search({
+        type: 'track',
+        query: userValue, limit: 20
+    }, function (err, data) {
+        if (err) {
+            console.log('Spotify Error occurred: ' + err);
+            return;
+        }
 
-    for (var i = 0; i < songs.length; i++) {
-      data.push({
-        'artist(s)': songs[i].artists.map(getArtistNames),
-        'song name: ': songs[i].name,
-        'preview song: ': songs[i].preview_url,
-        'album: ': songs[i].album.name,
-      });
-    }
-    console.log(data);
-   
-  });
+        var songs = data.tracks.items;
+        var data = []; //empty array to hold data
+
+        for (var i = 0; i < songs.length; i++) {
+            data.push({
+                'artist(s)': songs[i].artists.map(getArtistNames),
+                'song name: ': songs[i].name,
+                'preview song: ': songs[i].preview_url,
+                'album: ': songs[i].album.name,
+            });
+        }
+        console.log(data);
+        console.log(songs);
+
+    });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
