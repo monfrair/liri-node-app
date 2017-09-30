418c71b77d2d2651c415d55b983bc3109f69a478
Bill Sorrentino <williamsorrentino@gmail.com>
2017-09-30 10:07:37 -0400

    update to js files

--- liri.js
+++ liri.js
@@ -24,44 +24,32 @@ var nodeArgs = process.argv;
 
 // Command Instructions possibilities: my-tweets, spotify-this-song, movie-this, do-what-it-says
 var userChoice = process.argv[2];
-var userValue = process.argv[3];  //will this handle the whole name of the song if multiple words?
+var userValue = process.argv[3]; //will this handle the whole name of the song if multiple words?
 
-for(i=4; i<process.argv.length; i++){
-	    userValue += '+' + process.argv[i];
-	}
+for (i = 4; i < process.argv.length; i++) {
+    userValue += '+' + process.argv[i];
+}
 console.log(userValue);
 console.log(userChoice);
 
-var spotifyThisSong = new spotify({
-    id: 'cee40f14b4714e95a4942522c241ced1',
-    secret: '100ea93662344e14992be21b7a6d0b96',
-});
 
 
-//case switch function to determine users choice  change to an if else statement
-function userEntry(){
-	
-	switch(userChoice){
 
-		case 'my-tweets':
-		fetchTweets();
-		break;
 
-		case 'spotify-this-song':
-		getSpotify();
-		break;
 
-		case 'movie-this':
-		aMovieForMe();
-		break;
-
-		case 'do-what-it-says':
-		followTheTextbook();
-		break;
-		
-        default: 
-            console.log("Please choose one of the following options: my-tweets,  spotify-this-song,  movie-this, or  do-what-it-says");
-	}
+if (userChoice === 'spotify-this-song' {
+        getSpotify();
+    } else if (userChoice === 'my-tweets') {
+        fetchTweets();
+    } else if (userChoice === 'movie-this') {
+        getMovie();
+    } else if (userChoice === 'do-what-it-says') {
+        whatItSays();
+    } else if (userChoice === 'spotify-this-song') {
+        getSpotify();
+    } else if (userChoice === " ") {
+        console.log('please choose one');
+    }
 };
 
 
@@ -71,13 +59,19 @@ function getSpotify() {
     if (userValue === undefined) {
         userValue = 'The Sign';
     };
-    
+
 
     //spotify variables for user account
+    var spotifyThisSong = new spotify({
+        clientid: 'cee40f14b4714e95a4942522c241ced1',
+        secret: '100ea93662344e14992be21b7a6d0b96',
+    });
 
-   spotify.search({
+
+    spotify.search({
         type: 'track',
-        query: userValue, limit: 20
+        query: userValue,
+        limit: 20
     }, function (err, data) {
         if (err) {
             console.log('Spotify Error occurred: ' + err);
@@ -99,4 +93,38 @@ function getSpotify() {
         console.log(songs);
 
     });
+
+
+    //if(command === "my-tweets"){
+    //          liri.myTweets().then(function(results){
+    //            resolve(results);
+    //          });
+    //        }
+    //        else if(command === "spotify-this-song"){
+    //          liri.spotifyThisSong(args).then(function(results){
+    //            resolve(results);
+    //          });
+    //        }
+    //        else if(command === "movie-this"){
+    //          liri.movieThis(args.replace(" ", "+")).then(function(results){
+    //            resolve(results);
+    //          });
+    //        }
+    //        else if(command === "do-what-it-says"){
+    //          //the recurse conditional makes sure that do-what-it-says isn't run from random.txt, which, I haven't tested it, but I think it would just be a neverending nightmare of recursion. So, that crisis is averted.
+    //          if(!recurse){
+    //            liri.doWhatItSays();
+    //            resolve("");
+    //          }
+    //          else{
+    //            resolve("ALERT: Command not executed; cannot call do-what-it-says from within random.txt");
+    //          }
+    //
+    //        }
+    //        else{
+    //          //if the command isn't recognized, the promise resolves with an alert for an unknown command. A reject may interfere with other commands being run, if it rejects in do-what-it-says
+    //          resolve("ALERT: Command not executed; could not recognize command");
+    //        }
+    //    });
+
 };
