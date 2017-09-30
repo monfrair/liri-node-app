8b9641ae435d1a5c8357cbddf3246b869ac1bfa8
Bill Sorrentino <williamsorrentino@gmail.com>
2017-09-30 19:41:24 -0400

    have the spotify api working correctly now

--- liri.js
+++ liri.js
@@ -5,6 +5,9 @@
 // * do-what-it-says
 
 
+////#########  error is in the for loop, check it out and fix it /////////////
+
+
 // NPM Package for reading and writing files
 var fs = require('fs');
 
@@ -24,31 +27,30 @@ var nodeArgs = process.argv;
 
 // Command Instructions possibilities: my-tweets, spotify-this-song, movie-this, do-what-it-says
 var userChoice = process.argv[2];
-var userValue = process.argv[3]; //will this handle the whole name of the song if multiple words?
+var userValue = " "; //will this handle the whole name of the song if multiple words?
 
-for (i = 4; i < process.argv.length; i++) {
-    userValue += '+' + process.argv[i];
+for (i = 3; i < process.argv.length; i++) {
+    userValue += ' ' + process.argv[i];
 }
-console.log(userValue);
-console.log(userChoice);
+//console.log(userValue);
+//console.log(userChoice);
+
 
 
 if (userChoice === 'spotify-this-song') {
-        getSpotify()
-    } else if (userChoice === 'my-tweets') {
-        fetchTweets();
-    } else if (userChoice === 'movie-this') {
-        getMovie()
-    } else if (userChoice === 'do-what-it-says') {
-        whatItSays()
-    } else if (userChoice === 'spotify-this-song') {
-        getSpotify()
-    } else if (userChoice === " ") {
-        console.log('please choose one')
-    }
+    getSpotify();
+} else if (userChoice === 'my-tweets') {
+    fetchTweets();
+} else if (userChoice === 'movie-this') {
+    getMovie();
+} else if (userChoice === 'do-what-it-says') {
+    whatItSays();
+} else   {
+    console.log('please choose one');
+}
 
 
-console.log(userChoice);
+
 //Function for finding songs on Spotify
 function getSpotify() {
     //If it doesn't find a song, default is The Sign by Ace of Base
@@ -63,20 +65,26 @@ function getSpotify() {
         secret: '100ea93662344e14992be21b7a6d0b96',
     });
 
+    
 
-    spotify.search({
+
+
+//    console.log(userValue);
+    spotifyThisSong.search({
         type: 'track',
         query: userValue,
         limit: 20
     }, function (err, data) {
         if (err) {
-            return console.log('Spotify Error occurred: ' + err);
-            
+            console.log('Spotify Error occurred: ' + err);
+            return;
         }
-        
-        console.log(data);
-    });
 
+        
+        var getArtistNames = function (artist) {
+        return artist.name;
+    };
+        
         var songs = data.tracks.items;
         var data = []; //empty array to hold data
 
@@ -89,41 +97,11 @@ function getSpotify() {
             });
         }
         console.log(data);
-        console.log(songs);
+        
 
-    };
-
-
-    //if(command === "my-tweets"){
-    //          liri.myTweets().then(function(results){
-    //            resolve(results);
-    //          });
-    //        }
-    //        else if(command === "spotify-this-song"){
-    //          liri.spotifyThisSong(args).then(function(results){
-    //            resolve(results);
-    //          });
-    //        }
-    //        else if(command === "movie-this"){
-    //          liri.movieThis(args.replace(" ", "+")).then(function(results){
-    //            resolve(results);
-    //          });
-    //        }
-    //        else if(command === "do-what-it-says"){
-    //          //the recurse conditional makes sure that do-what-it-says isn't run from random.txt, which, I haven't tested it, but I think it would just be a neverending nightmare of recursion. So, that crisis is averted.
-    //          if(!recurse){
-    //            liri.doWhatItSays();
-    //            resolve("");
-    //          }
-    //          else{
-    //            resolve("ALERT: Command not executed; cannot call do-what-it-says from within random.txt");
-    //          }
-    //
-    //        }
-    //        else{
-    //          //if the command isn't recognized, the promise resolves with an alert for an unknown command. A reject may interfere with other commands being run, if it rejects in do-what-it-says
-    //          resolve("ALERT: Command not executed; could not recognize command");
-    //        }
-    //    });
+    });
 
 
+    
+
+};
